b44669fb0dc564554cf82ffdbc0d265e15b61977
Patrick <gpatryllus@gmail.com>
2022-06-21 19:51:58 +0300

    added orderType control in createManifest form and added EID order type processing

--- omod/src/main/java/org/openmrs/module/kenyaemrorderentry/page/controller/orders/ManifestOrdersHomePageController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemrorderentry/page/controller/orders/ManifestOrdersHomePageController.java
@@ -2,6 +2,8 @@ package org.openmrs.module.kenyaemrorderentry.page.controller.orders;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
+import org.openmrs.Order;
+import org.openmrs.OrderSet;
 import org.openmrs.PatientIdentifierType;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.kenyacore.RegimenMappingUtils;
@@ -23,7 +25,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 @AppPage("kenyaemr.labmanifest")
 public class ManifestOrdersHomePageController {
@@ -34,10 +39,30 @@ public class ManifestOrdersHomePageController {
         List<LabManifestOrder> allOrdersForManifest = Context.getService(KenyaemrOrdersService.class).getLabManifestOrderByManifest(manifest);
         PatientIdentifierType pat = Utils.getUniquePatientNumberIdentifierType();
         LabOrderDataExchange e = new LabOrderDataExchange();
-        model.put("manifest", manifest);
-        model.put("manifestOrders", allOrdersForManifest);
-        model.put("eligibleOrders", e.getActiveViralLoadOrdersNotInManifest(null,manifest.getStartDate(),manifest.getEndDate()));
-        model.put("cccNumberType", pat.getPatientIdentifierTypeId());
+        Set<Order> activeOrdersNotManifested = new HashSet<Order>();
+        Integer orderType = null;
+        Set<Order> activeOrdersNotInManifest = new HashSet<Order>();
+        activeOrdersNotInManifest = e.getActiveOrdersNotInManifest(null, manifest.getStartDate(),manifest.getEndDate());
+
+       if(!activeOrdersNotInManifest.isEmpty()) {
+           for (Order o : activeOrdersNotInManifest) {
+               if (o.getPatient().getAge() >= 2) {   // this is a vl order
+                   activeOrdersNotManifested = e.getActiveViralLoadOrdersNotInManifest(null, manifest.getStartDate(), manifest.getEndDate());
+                   orderType = LabManifestOrder.VL_ORDER_TYPE;
+               }
+               else if(o.getPatient().getAge() < 2){  // this is a eid order
+                   activeOrdersNotManifested = e.getActiveEidOrdersNotInManifest(null, manifest.getStartDate(), manifest.getEndDate());
+                   orderType = LabManifestOrder.EID_ORDER_TYPE;
+               }
+           }
+
+           model.put("eligibleOrders", activeOrdersNotManifested );
+           model.put("orderType", orderType);
+           model.put("manifest", manifest);
+           model.put("manifestOrders", allOrdersForManifest);
+           model.put("cccNumberType", pat.getPatientIdentifierTypeId());
+        }
+
     }
 
 }
