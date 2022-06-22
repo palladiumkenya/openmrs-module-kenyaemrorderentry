419deaa414d72ed423c2aa8e83b541a7c1e33a3c
Patrick Waweru <patrick.waweru@thepalladiumgroup.com>
2022-06-22 12:57:30 +0300

    more tests

--- api/src/main/java/org/openmrs/module/kenyaemrorderentry/labDataExchange/LabwareSystemWebRequest.java
+++ api/src/main/java/org/openmrs/module/kenyaemrorderentry/labDataExchange/LabwareSystemWebRequest.java
@@ -5,6 +5,7 @@ import com.google.gson.*;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.StringEscapeUtils;
 import org.apache.commons.text.translate.UnicodeUnescaper;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -33,10 +34,14 @@ import org.openmrs.module.kenyaemrorderentry.util.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * An implementation for Labware
@@ -145,6 +150,19 @@ public class LabwareSystemWebRequest extends LabWebRequest {
         }
     }
 
+    public void testPull() {
+        System.out.println("Testing LAB Get Results");
+        List<Integer> myOrders = new ArrayList();
+        myOrders.add(1);
+        List<Integer> myManifests = new ArrayList();
+        try {
+            pullResult( myOrders,  myManifests);
+        } catch (Exception m) {
+            System.err.println("Lab Get Results Error: " + m.getMessage());
+            m.printStackTrace();
+        }
+    }
+
     public void pullResult(List<Integer> orderIds, List<Integer> manifestOrderIds) throws IOException {
 
         KenyaemrOrdersService kenyaemrOrdersService = Context.getService(KenyaemrOrdersService.class);
@@ -169,7 +187,8 @@ public class LabwareSystemWebRequest extends LabWebRequest {
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
         try {
             // CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-            String facilityCode = Utils.getDefaultLocationMflCode(Utils.getDefaultLocation());
+            //String facilityCode = Utils.getDefaultLocationMflCode(Utils.getDefaultLocation());
+            String facilityCode = "17747";
             String orderNumbers = StringUtils.join(orderIds, ",");
             URIBuilder builder = new URIBuilder(serverUrl);
             builder.addParameter("mfl_code", facilityCode);
@@ -187,91 +206,113 @@ public class LabwareSystemWebRequest extends LabWebRequest {
             final int statusCode = response.getStatusLine().getStatusCode();
             if (statusCode != 200) {
                 throw new RuntimeException("Get Lab Results Failed with HTTP error code : " + statusCode);
+            } else {
+                System.out.println("Get Lab Results: Success");
             }
 
-            String responseStringRaw = EntityUtils.toString(response.getEntity());
-            String responseStringEscape = StringEscapeUtils.escapeJava(responseStringRaw);
-            String strippedUnicodeChars = new UnicodeUnescaper().translate(responseStringEscape);
+            //Testing
+            String jsonString = null;
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                BufferedReader rd = new BufferedReader(new InputStreamReader(entity.getContent()));
 
-            String finalChars = StringEscapeUtils.unescapeJava(strippedUnicodeChars);
-            String removeBackslash = finalChars.replace("\\", "");
-
-
-            Gson gson = new GsonBuilder().serializeNulls().create();
-            JsonElement rootNode = gson.fromJson(finalChars, JsonElement.class);
-
-            JsonArray resultArray = null;
-            if(rootNode.isJsonObject()){
-                JsonObject jsonObject = rootNode.getAsJsonObject();
-                JsonElement vlResultArray = jsonObject.get("data");
-                if(vlResultArray.isJsonArray()){
-                    resultArray = vlResultArray.getAsJsonArray();
+                try {
+                    jsonString = rd.lines().collect(Collectors.joining()).toString();
+                    System.out.println("Lab Results Get Request JSON -> " + jsonString);
+                } finally {
+                    rd.close();
                 }
             }
 
-            JsonArray cleanedArray = new JsonArray();
-            if (resultArray != null && !resultArray.isEmpty()) {
-                for (int i =0; i < resultArray.size(); i++) {
-                    JsonObject result = resultArray.get(i).getAsJsonObject();
-                    result.addProperty("full_names", "Replaced Name"); // this is a short workaround to handle data coming from eid/vl system and were pushed from kenyaemr with unicode literals
-                    cleanedArray.add(result);
-
-                }
-            }
+            JSONParser parser = new JSONParser();
+            JSONObject responseObj = (JSONObject) parser.parse(jsonString);
+            //JSONObject errorObj = (JSONObject) responseObj.get("error");
 
 
-            if (resultArray != null && !resultArray.isEmpty()) {
-                String json = gson.toJson(cleanedArray);
-                ProcessViralLoadResults.processPayload(json);// the only way that works for now is posting this through REST
 
-                // update manifest details appropriately for the next execution
-                String [] incompleteStatuses = new String []{"Incomplete"};
-                if (manifestToUpdateResults != null) {
-                    List<LabManifestOrder> pendingResultsForNextIteration = kenyaemrOrdersService.getLabManifestOrderByManifestAndStatus(manifestToUpdateResults, "Sent");
-                    List<LabManifestOrder> incompleteResults = kenyaemrOrdersService.getLabManifestOrderByManifestAndStatus(manifestToUpdateResults, incompleteStatuses);
-
-                    if (pendingResultsForNextIteration.size() < 1 && incompleteResults.size() < 1) {
-                        manifestToUpdateResults.setStatus("Complete results");
-                        manifestToUpdateResults.setDateChanged(new Date());
-                        kenyaemrOrdersService.saveLabOrderManifest(manifestToUpdateResults);
-
-                        gpLastProcessedManifest.setPropertyValue(""); // set value to null so that the execution gets to the next manifest
-                        Context.getAdministrationService().saveGlobalProperty(gpLastProcessedManifest);
-                    } else if (pendingResultsForNextIteration.size() < 1 && incompleteResults.size() > 0) {
-                        manifestToUpdateResults.setStatus("Incomplete results");
-                        manifestToUpdateResults.setDateChanged(new Date());
-                        kenyaemrOrdersService.saveLabOrderManifest(manifestToUpdateResults);
-
-                        gpLastProcessedManifest.setPropertyValue(""); // set value to null so that the execution gets to the next manifest
-                        Context.getAdministrationService().saveGlobalProperty(gpLastProcessedManifest);
-                    }
-
-                    // update manifest global property
-                    if (pendingResultsForNextIteration.size() > 0) {
-                        gpLastProcessedManifest.setPropertyValue(manifestToUpdateResults.getId().toString());
-                        gpLastProcessedManifestUpdatetime.setPropertyValue(Utils.getSimpleDateFormat(LabOrderDataExchange.MANIFEST_LAST_UPDATE_PATTERN).format(new Date()));
-                        Context.getAdministrationService().saveGlobalProperty(gpLastProcessedManifest);
-                        Context.getAdministrationService().saveGlobalProperty(gpLastProcessedManifestUpdatetime);
-                    }
-                }
-
-            }
-
-            // check and mark status for all orders that may have not been found in the lab system. It is still not clear how this happens since the records are marked as sent in the manifest
-
-            Integer[] intArray = new Integer[manifestOrderIds.size()];
-            intArray = manifestOrderIds.toArray(intArray);
-
-            List<LabManifestOrder> ordersNotInLabSystem = kenyaemrOrdersService.getLabManifestOrderByNotFoundInLabSystem(intArray);
-
-            for (LabManifestOrder o : ordersNotInLabSystem) {
-                o.setStatus("Record not found");
-                o.setDateChanged(new Date());
-                kenyaemrOrdersService.saveLabManifestOrder(o);
-            }
-
-            System.out.println("Successfully executed the task that pulls lab requests");
-            log.info("Successfully executed the task that pulls lab requests");
+//            String responseStringRaw = EntityUtils.toString(response.getEntity());
+//            String responseStringEscape = StringEscapeUtils.escapeJava(responseStringRaw);
+//            String strippedUnicodeChars = new UnicodeUnescaper().translate(responseStringEscape);
+//
+//            String finalChars = StringEscapeUtils.unescapeJava(strippedUnicodeChars);
+//            String removeBackslash = finalChars.replace("\\", "");
+//
+//
+//            Gson gson = new GsonBuilder().serializeNulls().create();
+//            JsonElement rootNode = gson.fromJson(finalChars, JsonElement.class);
+//
+//            JsonArray resultArray = null;
+//            if(rootNode.isJsonObject()){
+//                JsonObject jsonObject = rootNode.getAsJsonObject();
+//                JsonElement vlResultArray = jsonObject.get("data");
+//                if(vlResultArray.isJsonArray()){
+//                    resultArray = vlResultArray.getAsJsonArray();
+//                }
+//            }
+//
+//            JsonArray cleanedArray = new JsonArray();
+//            if (resultArray != null && !resultArray.isEmpty()) {
+//                for (int i =0; i < resultArray.size(); i++) {
+//                    JsonObject result = resultArray.get(i).getAsJsonObject();
+//                    result.addProperty("full_names", "Replaced Name"); // this is a short workaround to handle data coming from eid/vl system and were pushed from kenyaemr with unicode literals
+//                    cleanedArray.add(result);
+//
+//                }
+//            }
+//
+//
+//            if (resultArray != null && !resultArray.isEmpty()) {
+//                String json = gson.toJson(cleanedArray);
+//                ProcessViralLoadResults.processPayload(json);// the only way that works for now is posting this through REST
+//
+//                // update manifest details appropriately for the next execution
+//                String [] incompleteStatuses = new String []{"Incomplete"};
+//                if (manifestToUpdateResults != null) {
+//                    List<LabManifestOrder> pendingResultsForNextIteration = kenyaemrOrdersService.getLabManifestOrderByManifestAndStatus(manifestToUpdateResults, "Sent");
+//                    List<LabManifestOrder> incompleteResults = kenyaemrOrdersService.getLabManifestOrderByManifestAndStatus(manifestToUpdateResults, incompleteStatuses);
+//
+//                    if (pendingResultsForNextIteration.size() < 1 && incompleteResults.size() < 1) {
+//                        manifestToUpdateResults.setStatus("Complete results");
+//                        manifestToUpdateResults.setDateChanged(new Date());
+//                        kenyaemrOrdersService.saveLabOrderManifest(manifestToUpdateResults);
+//
+//                        gpLastProcessedManifest.setPropertyValue(""); // set value to null so that the execution gets to the next manifest
+//                        Context.getAdministrationService().saveGlobalProperty(gpLastProcessedManifest);
+//                    } else if (pendingResultsForNextIteration.size() < 1 && incompleteResults.size() > 0) {
+//                        manifestToUpdateResults.setStatus("Incomplete results");
+//                        manifestToUpdateResults.setDateChanged(new Date());
+//                        kenyaemrOrdersService.saveLabOrderManifest(manifestToUpdateResults);
+//
+//                        gpLastProcessedManifest.setPropertyValue(""); // set value to null so that the execution gets to the next manifest
+//                        Context.getAdministrationService().saveGlobalProperty(gpLastProcessedManifest);
+//                    }
+//
+//                    // update manifest global property
+//                    if (pendingResultsForNextIteration.size() > 0) {
+//                        gpLastProcessedManifest.setPropertyValue(manifestToUpdateResults.getId().toString());
+//                        gpLastProcessedManifestUpdatetime.setPropertyValue(Utils.getSimpleDateFormat(LabOrderDataExchange.MANIFEST_LAST_UPDATE_PATTERN).format(new Date()));
+//                        Context.getAdministrationService().saveGlobalProperty(gpLastProcessedManifest);
+//                        Context.getAdministrationService().saveGlobalProperty(gpLastProcessedManifestUpdatetime);
+//                    }
+//                }
+//
+//            }
+//
+//            // check and mark status for all orders that may have not been found in the lab system. It is still not clear how this happens since the records are marked as sent in the manifest
+//
+//            Integer[] intArray = new Integer[manifestOrderIds.size()];
+//            intArray = manifestOrderIds.toArray(intArray);
+//
+//            List<LabManifestOrder> ordersNotInLabSystem = kenyaemrOrdersService.getLabManifestOrderByNotFoundInLabSystem(intArray);
+//
+//            for (LabManifestOrder o : ordersNotInLabSystem) {
+//                o.setStatus("Record not found");
+//                o.setDateChanged(new Date());
+//                kenyaemrOrdersService.saveLabManifestOrder(o);
+//            }
+//
+//            System.out.println("Successfully executed the task that pulls lab requests");
+//            log.info("Successfully executed the task that pulls lab requests");
         } catch (Exception e) {
             System.err.println("Get Lab Results Error: " + e.getMessage());
             e.printStackTrace();
