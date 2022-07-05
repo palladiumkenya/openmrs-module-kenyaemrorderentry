49ea4818cd57055a3aa3c082467150f9e7552774
Patrick Waweru <patrick.waweru@thepalladiumgroup.com>
2022-07-05 23:05:14 +0300

    We have two systems CHAI and LABWARE. Each with EID and VL. Lets fix this

--- api/src/main/java/org/openmrs/module/kenyaemrorderentry/labDataExchange/LabwareSystemWebRequest.java
+++ api/src/main/java/org/openmrs/module/kenyaemrorderentry/labDataExchange/LabwareSystemWebRequest.java
@@ -17,9 +17,12 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.openmrs.GlobalProperty;
@@ -99,19 +102,19 @@ public class LabwareSystemWebRequest extends LabWebRequest {
             API_KEY = gpVLApiToken.getPropertyValue().trim();
         }
 
-        // SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
-        //         SSLContexts.createDefault(),
-        //         new String[]{"TLSv1.2"},
-        //         null,
-        //         SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                SSLContexts.createDefault(),
+                new String[]{"TLSv1.2"},
+                null,
+                SSLConnectionSocketFactory.getDefaultHostnameVerifier());
 
-        // CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
-        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+        //CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 
         try {
 
             //Define a postRequest request
-            System.out.println("Lab Results POST: Server URL: " + serverUrl);
+            System.out.println("Labware Lab Results POST: Server URL: " + serverUrl);
             HttpPost postRequest = new HttpPost(serverUrl);
 
             //Set the API media type in http content-type header
@@ -120,7 +123,7 @@ public class LabwareSystemWebRequest extends LabWebRequest {
 
             //Set the request post body
             String payload = manifestOrder.getPayload();
-            System.out.println("Lab Results POST: Server Payload: " + payload);
+            System.out.println("Labware Lab Results POST: Server Payload: " + payload);
             StringEntity userEntity = new StringEntity(payload);
             postRequest.setEntity(userEntity);
 
@@ -130,37 +133,27 @@ public class LabwareSystemWebRequest extends LabWebRequest {
             int statusCode = response.getStatusLine().getStatusCode();
 
             if (statusCode == 429) { // too many requests. just terminate
-                System.out.println("Lab Results POST: 429 The push lab scheduler has been configured to run at very short intervals. Please change this to at least 30min");
-                log.warn("Lab Results POST: 429 The push scheduler has been configured to run at very short intervals. Please change this to at least 30min");
+                System.out.println("Labware Lab Results POST: 429 The push lab scheduler has been configured to run at very short intervals. Please change this to at least 30min");
+                log.warn("Labware Lab Results POST: 429 The push scheduler has been configured to run at very short intervals. Please change this to at least 30min");
                 return(false);
             }
 
             if (statusCode != 201 && statusCode != 200 && statusCode != 422 && statusCode != 403) { // skip for status code 422: unprocessable entity, and status code 403 for forbidden response
-                // JSONParser parser = new JSONParser();
-                // JSONObject responseObj = (JSONObject) parser.parse(EntityUtils.toString(response.getEntity()));
-                // JSONObject errorObj = (JSONObject) responseObj.get("errors");
-                // manifestOrder.setStatus("Error - " + statusCode + ". Msg" + errorObj.get("message"));
                 manifestOrder.setStatus("Pending");
                 kenyaemrOrdersService.saveLabManifestOrder(manifestOrder);
-                System.out.println("Lab Results POST: There was an error sending lab id = " + manifestOrder.getId() + " Status: " + statusCode);
-                log.warn("Lab Results POST: There was an error sending lab id = " + manifestOrder.getId() + " Status: " + statusCode);
-                // throw new RuntimeException("Failed with HTTP error code : " + statusCode + ". Error msg: " + errorObj.get("message"));
+                System.out.println("Labware Lab Results POST: There was an error sending lab id = " + manifestOrder.getId() + " Status: " + statusCode);
+                log.warn("Labware Lab Results POST: There was an error sending lab id = " + manifestOrder.getId() + " Status: " + statusCode);
                 return(false);
             } else if (statusCode == 403 || statusCode == 422) {
-                // JSONParser parser = new JSONParser();
-                // JSONObject responseObj = (JSONObject) parser.parse(EntityUtils.toString(response.getEntity()));
-                // JSONObject errorObj = (JSONObject) responseObj.get("errors");
-                // System.out.println("Lab Results POST: Error while submitting manifest sample. " + "Error - " + statusCode + ". Msg" + errorObj.get("message"));
-                // log.error("Lab Results POST: Error while submitting manifest sample. " + "Error - " + statusCode + ". Msg" + errorObj.get("message"));
                 manifestOrder.setStatus("Pending");
                 kenyaemrOrdersService.saveLabManifestOrder(manifestOrder);
-                System.out.println("Lab Results POST: There was an error sending lab id = " + manifestOrder.getId() + " Status: " + statusCode);
-                log.warn("Lab Results POST: There was an error sending lab id = " + manifestOrder.getId() + " Status: " + statusCode);
+                System.out.println("Labware Lab Results POST: There was an error sending lab id = " + manifestOrder.getId() + " Status: " + statusCode);
+                log.warn("Labware Lab Results POST: There was an error sending lab id = " + manifestOrder.getId() + " Status: " + statusCode);
                 return(false);
             } else if (statusCode == 201 || statusCode == 200) {
                 manifestOrder.setStatus("Sent");
-                System.out.println("Lab Results POST: Successfully pushed a VL lab test id " + manifestOrder.getId());
-                log.info("Lab Results POST: Successfully pushed a VL lab test id " + manifestOrder.getId());
+                System.out.println("Labware Lab Results POST: Successfully pushed a VL lab test id " + manifestOrder.getId());
+                log.info("Labware Lab Results POST: Successfully pushed a VL lab test id " + manifestOrder.getId());
                 kenyaemrOrdersService.saveLabManifestOrder(manifestOrder);
 
                 if (toProcess != null && manifestStatus.equals("Ready to send")) {
@@ -169,33 +162,18 @@ public class LabwareSystemWebRequest extends LabWebRequest {
                 }
                 Context.flushSession();
 
-                System.out.println("Lab Results POST: Push Successfull");
+                System.out.println("Labware Lab Results POST: Push Successfull");
                 return(true);
             }
         } catch (Exception e) {
-            System.err.println("Lab Results POST: Could not push requests to the lab! " + e.getMessage());
-            log.error("Lab Results POST: Could not push requests to the lab! " + e.getMessage());
+            System.err.println("Labware Lab Results POST: Could not push requests to the lab! " + e.getMessage());
+            log.error("Labware Lab Results POST: Could not push requests to the lab! " + e.getMessage());
             e.printStackTrace();
         } finally {
             httpClient.close();
         }
 
         return(false);
-    }
-
-    public void testPull() {
-        System.out.println("Testing LAB Get Results");
-        // List<Integer> myOrders = new ArrayList();
-        // myOrders.add(1);
-        // myOrders.add(19536);
-        // myOrders.add(56577);
-        // List<Integer> myManifests = new ArrayList();
-        // try {
-        //     pullResult( myOrders, myManifests, new LabManifest());
-        // } catch (Exception m) {
-        //     System.err.println("Lab Get Results Error: " + m.getMessage());
-        //     m.printStackTrace();
-        // }
     }
 
     public void pullResult(List<Integer> orderIds, List<Integer> manifestOrderIds, LabManifest manifestToUpdateResults) throws IOException {
@@ -220,29 +198,25 @@ public class LabwareSystemWebRequest extends LabWebRequest {
         GlobalProperty gpLastProcessedManifest = Context.getAdministrationService().getGlobalPropertyObject(LabOrderDataExchange.GP_MANIFEST_LAST_PROCESSED);
         GlobalProperty gpLastProcessedManifestUpdatetime = Context.getAdministrationService().getGlobalPropertyObject(LabOrderDataExchange.GP_MANIFEST_LAST_UPDATETIME);
 
-        // String serverUrl = gpServerUrl.getPropertyValue().trim();
-        // String API_KEY = gpApiToken.getPropertyValue().trim();
-        // LabManifest manifestToUpdateResults = null;
 
-//Not using SSL
-//        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
-//                SSLContexts.createDefault(),
-//                new String[]{"TLSv1.2"},
-//                null,
-//                SSLConnectionSocketFactory.getDefaultHostnameVerifier());
-//
-//        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
-        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        //Using SSL
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                SSLContexts.createDefault(),
+                new String[]{"TLSv1.2"},
+                null,
+                SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+        //CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
         try {
-            // CloseableHttpClient httpClient = HttpClientBuilder.create().build();
             String facilityCode = Utils.getDefaultLocationMflCode(Utils.getDefaultLocation());
-            //String facilityCode = "17747";
             String orderNumbers = StringUtils.join(orderIds, ",");
             URIBuilder builder = new URIBuilder(serverUrl);
             builder.addParameter("mfl_code", facilityCode);
             builder.addParameter("order_no", orderNumbers);
             URI uri = builder.build();
-            System.out.println("Get Lab Results URL: " + uri);
+            System.out.println("Get Labware Lab Results URL: " + uri);
 
             HttpGet httpget = new HttpGet(uri);
             httpget.addHeader("content-type", "application/x-www-form-urlencoded");
@@ -253,12 +227,11 @@ public class LabwareSystemWebRequest extends LabWebRequest {
 
             final int statusCode = response.getStatusLine().getStatusCode();
             if (statusCode != 200) {
-                throw new RuntimeException("Get Lab Results Failed with HTTP error code : " + statusCode);
+                throw new RuntimeException("Get Labware Lab Results Failed with HTTP error code : " + statusCode);
             } else {
-                System.out.println("Get Lab Results: REST Call Success");
+                System.out.println("Get Labware Lab Results: REST Call Success");
             }
 
-//            //Testing
            String jsonString = null;
            HttpEntity entity = response.getEntity();
            if (entity != null) {
@@ -266,70 +239,24 @@ public class LabwareSystemWebRequest extends LabWebRequest {
 
                try {
                    jsonString = rd.lines().collect(Collectors.joining()).toString();
-                   System.out.println("Lab Results Get: Request JSON -> " + jsonString);
+                   System.out.println("Labware Lab Results Get: Request JSON -> " + jsonString);
                } finally {
                    rd.close();
                }
            }
 
             JSONParser parser = new JSONParser();
-            // JSONObject responseObj = (JSONObject) parser.parse(jsonString);
-            ////JSONObject errorObj = (JSONObject) responseObj.get("error");
             JSONArray responseArray = (JSONArray) parser.parse(jsonString);
 
-            // String responseStringRaw = EntityUtils.toString(response.getEntity());
-            // String responseStringEscape = StringEscapeUtils.escapeJava(responseStringRaw);
-            // String strippedUnicodeChars = new UnicodeUnescaper().translate(responseStringEscape);
-
-            // String finalChars = StringEscapeUtils.unescapeJava(strippedUnicodeChars);
-            // String removeBackslash = finalChars.replace("\\", "");
-
-
-            // Gson gson = new GsonBuilder().serializeNulls().create();
-            // JsonElement rootNode = gson.fromJson(finalChars, JsonElement.class);
-
-            // JsonArray resultArray = null;
-            // if(rootNode.isJsonObject()){
-            //     JsonObject jsonObject = rootNode.getAsJsonObject();
-            //     JsonElement vlResultArray = jsonObject.get("data");
-            //     if(vlResultArray.isJsonArray()){
-            //         resultArray = vlResultArray.getAsJsonArray();
-            //     }
-            // }
-
-            // JsonArray cleanedArray = new JsonArray();
-            // if (resultArray != null && !resultArray.isEmpty()) {
-            //     for (int i =0; i < resultArray.size(); i++) {
-            //         JsonObject result = resultArray.get(i).getAsJsonObject();
-            //         result.addProperty("full_names", "Replaced Name"); // this is a short workaround to handle data coming from eid/vl system and were pushed from kenyaemr with unicode literals
-            //         cleanedArray.add(result);
-
-            //     }
-            // }
-
-
-            // if (resultArray != null && !resultArray.isEmpty()) {
-            //     System.out.println("Get Lab Results: Error: Got null/empty json results");
-            //     String json = gson.toJson(cleanedArray);
-            //     ProcessViralLoadResults.processPayload(json);// the only way that works for now is posting this through REST
             if (responseArray != null && !responseArray.isEmpty()) {
                 // update orders
-                // LabOrderDataExchange lode = new LabOrderDataExchange();
-                // lode.processIncomingViralLoadLabResults(jsonString);
                 ProcessViralLoadResults.processPayload(jsonString);
+
                 // update manifest details appropriately for the next execution
                 String [] incompleteStatuses = new String []{"Incomplete", "Pending", "Sending"};
 
-                //update manifests
-                // manifestToUpdateResults = kenyaemrOrdersService.getLabManifestOrderById(manifestOrderIds.get(0)).getLabManifest();
-                // kenyaemrOrdersService.getLabManifestOrderByOrderId(orderIds.get(0));
-                // OrderService.getOrder(7);
-
-                //OrderService orderService = Context.getOrderService();
-                //manifestToUpdateResults = kenyaemrOrdersService.getLabManifestOrderByOrderId(orderService.getOrder(orderIds.get(0))).getLabManifest();
-
                 if (manifestToUpdateResults != null) {
-                    System.out.println("Lab Results Get: Updating manifest with status");
+                    System.out.println("Labware Lab Results Get: Updating manifest with status");
                     List<LabManifestOrder> pendingResultsForNextIteration = kenyaemrOrdersService.getLabManifestOrderByManifestAndStatus(manifestToUpdateResults, "Sent");
                     List<LabManifestOrder> incompleteResults = kenyaemrOrdersService.getLabManifestOrderByManifestAndStatus(manifestToUpdateResults, incompleteStatuses);
 
@@ -342,7 +269,7 @@ public class LabwareSystemWebRequest extends LabWebRequest {
 
                         gpLastProcessedManifest.setPropertyValue(""); // set value to null so that the execution gets to the next manifest
                         Context.getAdministrationService().saveGlobalProperty(gpLastProcessedManifest);
-                        System.out.println("Lab Results Get: Updating manifest with status: Complete Results");
+                        System.out.println("Labware Lab Results Get: Updating manifest with status: Complete Results");
                     } else if (pendingResultsForNextIteration.size() < 1 && incompleteResults.size() > 0) {
                         manifestToUpdateResults.setStatus("Incomplete results");
                         manifestToUpdateResults.setDateChanged(new Date());
@@ -350,12 +277,12 @@ public class LabwareSystemWebRequest extends LabWebRequest {
 
                         gpLastProcessedManifest.setPropertyValue(""); // set value to null so that the execution gets to the next manifest
                         Context.getAdministrationService().saveGlobalProperty(gpLastProcessedManifest);
-                        System.out.println("Lab Results Get: Updating manifest with status: Incomplete Results");
+                        System.out.println("Labware Lab Results Get: Updating manifest with status: Incomplete Results");
                     }
 
                     // update manifest global property
                     if (pendingResultsForNextIteration.size() > 0) {
-                        System.out.println("Lab Results Get: Updating manifest global property");
+                        System.out.println("Labware Lab Results Get: Updating manifest global property");
                         gpLastProcessedManifest.setPropertyValue(manifestToUpdateResults.getId().toString());
                         gpLastProcessedManifestUpdatetime.setPropertyValue(Utils.getSimpleDateFormat(LabOrderDataExchange.MANIFEST_LAST_UPDATE_PATTERN).format(new Date()));
                         Context.getAdministrationService().saveGlobalProperty(gpLastProcessedManifest);
@@ -378,12 +305,12 @@ public class LabwareSystemWebRequest extends LabWebRequest {
                 kenyaemrOrdersService.saveLabManifestOrder(o);
             }
 
-            System.out.println("Lab Results Get: Successfully executed the task that pulls lab requests");
-            log.info("Lab Results Get: Successfully executed the task that pulls lab requests");
+            System.out.println("Labware Lab Results Get: Successfully executed the task that pulls lab requests");
+            log.info("Labware Lab Results Get: Successfully executed the task that pulls lab requests");
 
-            System.out.println("Lab Results Get: Successfully Done");
+            System.out.println("Labware Lab Results Get: Successfully Done");
         } catch (Exception e) {
-            System.err.println("Get Lab Results Error: " + e.getMessage());
+            System.err.println("Get Labware Lab Results Error: " + e.getMessage());
             e.printStackTrace();
         } finally {
             httpClient.close();
