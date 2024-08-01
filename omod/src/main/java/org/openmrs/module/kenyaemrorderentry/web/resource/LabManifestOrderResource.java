bd515d8a086cecd12fbc593c00ef08ca0018cc8e
Patrick Waweru <patrick.waweru@thepalladiumgroup.com>
2024-08-01 17:13:51 +0300

    3.x endpoints: Added payload to manifest order and added print endpoints

--- omod/src/main/java/org/openmrs/module/kenyaemrorderentry/web/resource/LabManifestOrderResource.java
+++ omod/src/main/java/org/openmrs/module/kenyaemrorderentry/web/resource/LabManifestOrderResource.java
@@ -1,7 +1,17 @@
 package org.openmrs.module.kenyaemrorderentry.web.resource;
 
+import java.util.Date;
+
+import org.openmrs.Order;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.kenyaemrorderentry.ModuleConstants;
 import org.openmrs.module.kenyaemrorderentry.api.service.KenyaemrOrdersService;
+import org.openmrs.module.kenyaemrorderentry.labDataExchange.ChaiSystemWebRequest;
+import org.openmrs.module.kenyaemrorderentry.labDataExchange.EdarpSystemWebRequest;
+import org.openmrs.module.kenyaemrorderentry.labDataExchange.LabOrderDataExchange;
+import org.openmrs.module.kenyaemrorderentry.labDataExchange.LabWebRequest;
+import org.openmrs.module.kenyaemrorderentry.labDataExchange.LabwareSystemWebRequest;
+import org.openmrs.module.kenyaemrorderentry.manifest.LabManifest;
 import org.openmrs.module.kenyaemrorderentry.manifest.LabManifestOrder;
 import org.openmrs.module.webservices.rest.web.RequestContext;
 import org.openmrs.module.webservices.rest.web.RestConstants;
@@ -14,9 +24,15 @@ import org.openmrs.module.webservices.rest.web.resource.api.PageableResult;
 import org.openmrs.module.webservices.rest.web.resource.impl.DataDelegatingCrudResource;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
 import org.openmrs.module.webservices.rest.web.resource.impl.NeedsPaging;
+import org.openmrs.module.webservices.rest.web.response.ResourceDoesNotSupportOperationException;
 import org.openmrs.module.webservices.rest.web.response.ResponseException;
+import org.openmrs.ui.framework.SimpleObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
 @Resource(name = RestConstants.VERSION_1 +  "/labmanifestorder", supportedClass = LabManifestOrder.class, supportedOpenmrsVersions = {"2.0.*", "2.1.*", "2.2.*", "2.0 - 2.*"})
@@ -29,9 +45,7 @@ public class LabManifestOrderResource extends DataDelegatingCrudResource<LabMani
 
     @Override
     protected void delete(LabManifestOrder labManifest, String reason, RequestContext context) throws ResponseException {
-        labManifest.setVoided(true);
-        labManifest.setVoidReason(reason);
-        Context.getService(KenyaemrOrdersService.class).saveLabManifestOrder(labManifest);
+        Context.getService(KenyaemrOrdersService.class).voidLabManifestOrder(labManifest.getId());
     }
 
     @Override
@@ -42,7 +56,37 @@ public class LabManifestOrderResource extends DataDelegatingCrudResource<LabMani
     @Override
     public LabManifestOrder save(LabManifestOrder labManifestOrder) {
         System.out.println("Saving a new manifest order: " + labManifestOrder.toString());
-        return Context.getService(KenyaemrOrdersService.class).saveLabManifestOrder(labManifestOrder);
+        // Generate the payload
+        LabManifest manifest = labManifestOrder.getLabManifest();
+        Order order = labManifestOrder.getOrder();
+        Date dateSampleCollected = labManifestOrder.getSampleCollectionDate();
+        Date dateSampleSeparated = labManifestOrder.getSampleSeparationDate();
+        String sampleType = labManifestOrder.getSampleType();
+        LabWebRequest payloadGenerator = null;
+
+        if (LabOrderDataExchange.getSystemType() == ModuleConstants.CHAI_SYSTEM) {
+            payloadGenerator = new ChaiSystemWebRequest();
+        } else if (LabOrderDataExchange.getSystemType() == ModuleConstants.LABWARE_SYSTEM) {
+            payloadGenerator = new LabwareSystemWebRequest();
+        } else if (LabOrderDataExchange.getSystemType() == ModuleConstants.EDARP_SYSTEM) {
+            payloadGenerator = new EdarpSystemWebRequest();
+        }
+
+        if (payloadGenerator == null) {
+            throw new ResourceDoesNotSupportOperationException("An error occured while adding sample to manifest: could not create payload generator: check system type");
+        }
+
+        payloadGenerator.setManifestType(manifest.getManifestType());
+        ObjectNode payload = payloadGenerator.completePostPayload(order, dateSampleCollected, dateSampleSeparated, sampleType, manifest.getIdentifier());
+
+        if (payload != null && !payload.isEmpty()) {
+            labManifestOrder.setPayload(payload.toString());
+            labManifestOrder.setStatus("Pending");
+
+            return Context.getService(KenyaemrOrdersService.class).saveLabManifestOrder(labManifestOrder);
+        } else {
+            throw new ResourceDoesNotSupportOperationException("An error occured while adding sample to manifest: could not create payload");
+        }
     }
 
     @Override
