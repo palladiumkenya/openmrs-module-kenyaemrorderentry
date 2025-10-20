a17cea9f589cfd40bb76df9979b895c8bcc01eae
patryllus <gpatryllus@gmail.com>
2025-10-20 12:04:38 +0300

    Fixed failing Lims pull scheduler from Labware

--- api/src/main/java/org/openmrs/module/kenyaemrorderentry/task/PullLimsFacilityWideLabResultsTask.java
+++ api/src/main/java/org/openmrs/module/kenyaemrorderentry/task/PullLimsFacilityWideLabResultsTask.java
@@ -11,6 +11,8 @@ import org.openmrs.module.kenyaemrorderentry.queue.LimsQueue;
 import org.openmrs.module.kenyaemrorderentry.queue.LimsQueueStatus;
 import org.openmrs.scheduler.tasks.AbstractTask;
 import org.openmrs.util.OpenmrsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URL;
@@ -20,68 +22,71 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * This task pulls Lims lab results from the lims lab system.
+ * Prepares payload and performs remote login to Labware system
  */
 public class PullLimsFacilityWideLabResultsTask extends AbstractTask {
-	private Log log = LogFactory.getLog(getClass());
+
+	private static final Logger log = LoggerFactory.getLogger(PullLimsFacilityWideLabResultsTask.class);
 	private String url = "http://www.google.com:80/index.html";
 
-	KenyaemrOrdersService orderService = Context.getService(KenyaemrOrdersService.class);
+
+	KenyaemrOrdersService kenyaemrOrdersService = Context.getService(KenyaemrOrdersService.class);
 
 	/**
 	 * @see AbstractTask#execute()
 	 */
 	public void execute() {
-		System.out.println("Get Lims Lab Results: PULL TASK Starting");
-
+		System.out.println("PULL Lims Lab Results and persist to EMR: PULL TASK Starting");
 		Context.openSession();
-		String limsIntegrationEnabled = "";
-		GlobalProperty enableLimsIntegration = Context.getAdministrationService().getGlobalPropertyObject(ModuleConstants.GP_ENABLE_LIMS_INTEGRATION);
-		limsIntegrationEnabled = enableLimsIntegration.getPropertyValue().trim();
-		if (limsIntegrationEnabled.equals("false")) {
-			return;
-		} else {
-			Date currentDate = new Date();
-			currentDate.setTime(System.currentTimeMillis());
-			Date startOfDayMidnight = new Date(currentDate.getTime() - (1000 * 60 * 60 * 24));
-			Date midnightDateTime = OpenmrsUtil.getLastMomentOfDay(startOfDayMidnight);
+		// check first if there is internet connectivity before pushing
+		try {
+			URLConnection connection = new URL(url).openConnection();
+			connection.connect();
+			try {
 
-			KenyaemrOrdersService kenyaemrOrdersService = Context.getService(KenyaemrOrdersService.class);
-			List<LimsQueue> testsWithPendingResults = kenyaemrOrdersService.getLimsQueueEntriesByStatus(LimsQueueStatus.SUBMITTED, midnightDateTime, null, false);
-			List<Integer> activeTestOrders = new ArrayList<>();
-			System.out.println("Lab tests pending results from LIMS: " + activeTestOrders.size());
-			if (testsWithPendingResults.size() > 0) {
-
-				for (LimsQueue queueEntry : testsWithPendingResults) {
-					activeTestOrders.add(queueEntry.getOrder().getOrderId());
-				}
-				if (activeTestOrders.size() < 1) {
-					System.out.println("LIMS-EMR integration: there are no lab tests with pending results");
+				String limsIntegrationEnabled = "";
+				GlobalProperty enableLimsIntegration = Context.getAdministrationService().getGlobalPropertyObject(ModuleConstants.GP_ENABLE_LIMS_INTEGRATION);
+				limsIntegrationEnabled = enableLimsIntegration.getPropertyValue().trim();
+				if (limsIntegrationEnabled.equals("false")) {
 					return;
-				}
-				try {
-					URLConnection connection = new URL(url).openConnection();
-					connection.connect();
-					try {
-						System.out.println("LIMS-EMR integration: lab tests with pending results:  " + activeTestOrders.size());
-						LimsSystemWebRequest.pullFacilityWideLimsLabResult(activeTestOrders);
+				} else {
+					Date currentDate = new Date();
+					currentDate.setTime(System.currentTimeMillis());
+					Date startOfDayMidnight = new Date(currentDate.getTime() - (1000 * 60 * 60 * 24));
+					Date midnightDateTime = OpenmrsUtil.getLastMomentOfDay(startOfDayMidnight);
 
-					} catch (Exception e) {
-						throw new IllegalArgumentException("Lab Results Get: Unable to execute task that pulls lab requests", e);
-					} finally {
-						Context.closeSession();
-					}
-				} catch (IOException ioe) {
+					KenyaemrOrdersService kenyaemrOrdersService = Context.getService(KenyaemrOrdersService.class);
+					List<LimsQueue> testsWithPendingResults = kenyaemrOrdersService.getLimsQueueEntriesByStatus(LimsQueueStatus.SUBMITTED, midnightDateTime, null, false);
+					List<Integer> activeTestOrders = new ArrayList<>();
+					System.out.println("Lab tests pending results from LIMS: " + activeTestOrders.size());
+					if (testsWithPendingResults.size() > 0) {
 
-					try {
-						String text = "LIMS-EMR integration: At " + new Date() + " there was an error reported connecting to the internet. ";
-						log.warn(text);
-					} catch (Exception e) {
-						log.error("LIMS-EMR integration: Failed to check internet connectivity", e);
+						for (LimsQueue queueEntry : testsWithPendingResults) {
+							activeTestOrders.add(queueEntry.getOrder().getOrderId());
+						}
+						if (activeTestOrders.size() < 1) {
+							System.out.println("LIMS-EMR integration: there are no lab tests with pending results");
+							return;
+						}
+
 					}
 				}
+
+			} catch (Exception e) {
+				throw new IllegalArgumentException("Lab Results PULL: Unable to execute task that pulls viral load lab manifest", e);
+			} finally {
+				Context.closeSession();
+			}
+		} catch (IOException ioe) {
+
+			try {
+				String text = "Lims Lab Results PULL: At " + new Date() + " there was an error reported connecting to the internet. Will not attempt pulling labware lab tests ";
+				System.err.println(text);
+				log.warn(text);
+			} catch (Exception e) {
+				System.err.println("Lims Lab Results PULL: Failed to check internet connectivity" + e.getMessage());
+				log.error("Lims lab Results POST: Failed to check internet connectivity", e);
 			}
 		}
 	}
-
 }
